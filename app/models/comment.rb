671b77bafb947e33679c2691eb0b256a9db5b406
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-21 23:25:19 +0100

    Creating models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comment_counter: post.comments.count)
+  end
 end
