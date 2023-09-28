d6089b3ba6a578b648749e244ad928dfa8aea727
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-28 11:31:15 +0100

    Create forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  helper_method :current_user
+
+  def set_current_user
+    user_id = params[:user_id]
+    @current_user = User.find_by(id: user_id)
+  end
+
+  def current_user
+    @current_user ||= User.first
+  end
+
+  def require_user
+    redirect_to root_path unless @current_user
+  end
 end
