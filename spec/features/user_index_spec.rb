79eb7840f17470a825f631d83d31394b0bbfa465
ticoniq_ <okolietochi@gmail>
2023-10-02 16:16:02 +0100

    Update user index and show spec

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+RSpec.describe 'User index', type: :feature do
+  before :each do
+    @user = User.create(name: 'Aima', photo: '
+https://picsum.photos/id/23/200', bio: 'web developer',
+                        post_counter: 0)
+    visit root_path
+  end
+  it 'shows the correct content' do
+    expect(page).to have_content('Welcome To User Page')
+  end
+  it 'shows the number of posts each user has written' do
+    expect(page).to have_content('Number of posts')
+  end
+  it 'When I click on a user it should redirect to user/show page' do
+    click_on 'Aima'
+    expect(page).to have_content 'Aima'
+  end
+  it ' should return the correct css ' do
+    expect(page).to have_css("img[src*='
+https://picsum.photos/id/23/200
+']")
+  end
+end
