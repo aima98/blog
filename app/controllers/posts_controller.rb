1bb8ae1a991faebacc0a3711e3fb9a38d395e36e
Oumaima Nejjari <oumanj1998@gmail.com>
2023-10-03 16:11:00 +0100

    Fixing stylelint errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,9 @@ class PostsController < ApplicationController
 
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
-                  .order(created_at: :desc)
-                  .offset((page - 1) * per_page)
-                  .limit(per_page)
+      .order(created_at: :desc)
+      .offset((page - 1) * per_page)
+      .limit(per_page)
 
     total_posts = @user.posts.count
     @total_pages = (total_posts.to_f / per_page).ceil
